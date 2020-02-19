f90d1d7098d6a2bccff40052247de6b6e9ac0cd1
amir <amirdehghanfar@gamil.com>
2020-02-19 18:36:45 +1030

    feat: add days method with sraty and end date parameter and timeFrame

---
+++ MyTestDateTime/Controllers/DateTimeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyTestDateTime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DateTimeController : ControllerBase
+    {
+
+        [HttpGet("day/{start}/{end}/{timeFrame?}")]
+        public int days(DateTimeOffset start, DateTimeOffset end, TimeFrame? timeFrame = null)
+        {
+            int days = (int)(end - start).TotalDays;
+
+            switch (timeFrame)
+            {
+                case TimeFrame.Second:
+                    return (days * 24 * 60 * 60);
+                case TimeFrame.Minute:
+                    return (days * 24 * 60);
+                case TimeFrame.Hour:
+                    return (days * 24);
+                case TimeFrame.Year:
+                    return (days / 365);
+                default:
+                    break;
+            }
+            return (int)days;
+        }
+    }
+}
---
+++ MyTestDateTime/TimeScale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTestDateTime
+{
+    public enum TimeFrame
+    {
+        Second,
+        Minute,
+        Hour,
+        Year
+    }
+}
